666498cd34eed7ecc67fbae94cca9a5e92c0f8c6
Guide <70282836+mobahaa2001@users.noreply.github.com>
2022-09-05 15:15:04 +0200

    Add files via upload

---
+++ JavaScriptDataStructure/1-Fundamentals/4-ArraysVsSets.js
@@ -0,0 +1,12 @@
+// Arrays
+// 1- You can always use arrays --> it's a highly flexible data structure and you can use arrays for any kinds of data and that's why in probably 99% of all scripts, you only see arrays if you have lists of data.
+// 2- Must-use if order matters and/ or duplicates are wanted
+
+
+
+
+
+
+// Sets
+// 1- Only usable if order does not matter and you only need unique values.
+// 2- Can simplify data access (e.g.finding, deletion) compared to arrays.
