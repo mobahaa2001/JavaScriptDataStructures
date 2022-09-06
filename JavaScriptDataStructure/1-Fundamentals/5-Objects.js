06cc1b79cc666ea3a4a6f58b3f1642788f93fdfa
Guide <70282836+mobahaa2001@users.noreply.github.com>
2022-09-06 16:08:20 +0200

    Update 5-Objects.js

--- JavaScriptDataStructure/1-Fundamentals/5-Objects.js
+++ JavaScriptDataStructure/1-Fundamentals/5-Objects.js
@@ -11,12 +11,12 @@
 
 //////////////-------Notes--------////////////////
 
-// Object in general are unordered key value pairs of data.
-// The element access is done with help of the key name, So of the property name.
-// Objects are not really Iterable, There is the for-in loop which is built for objects, But you won't be able to use the for-of loop, you won't be able to access the values with help of some index or anything like that.
-// Keys are unique, values are not... So every key name must only appear once, You can't have two age properties in the same object.
-// Keys your properties actually have to be strings, numbers or symbols in JavaScript.
-// Object in general, therefore can be summarized as Data Structures or as constructs that can store Data, But that also can contain extra functionalities.
+// --> Object in general are unordered key value pairs of data.
+// --> The element access is done with help of the key name, So of the property name.
+// --> Objects are not really Iterable, There is the for-in loop which is built for objects, But you won't be able to use the for-of loop, you won't be able to access the values with help of some index or anything like that.
+// --> Keys are unique, values are not... So every key name must only appear once, You can't have two age properties in the same object.
+// --> Keys your properties actually have to be strings, numbers or symbols in JavaScript.
+// --> Object in general, therefore can be summarized as Data Structures or as constructs that can store Data, But that also can contain extra functionalities.
 
 /////////////--------Time Work----------/////////////
 
